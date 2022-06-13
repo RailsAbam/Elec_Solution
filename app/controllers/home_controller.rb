d481864c1eb4b892c8c1855fcd310cdf2e17d882
Rails Abam <abamsunday5@gmail.com>
2022-06-13 19:54:32 +0100

    commiting inverters file,solar,batteries, install, inquires files

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,31 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+
+  end
+
+  def batterie
+
+  end
+
+  def inverters
+
+  end
+
+  def inquire
+
+  end
+
+  def install
+
+  end
+
+
+  def solar
+
+  end
+
+
+end
