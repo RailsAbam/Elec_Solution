c23905e8e77794910de137914a7d327de4f6b036
Rails Abam <abamsunday5@gmail.com>
2022-06-27 17:45:29 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :elec_solutions
 
-  root "home#index"
+  root "home#home"
   get "home/batteries"
   get "home/solar"
   get "home/inverters"
   get "home/install"
   get "home/inquire"
+  get "home/index"
 
 end
